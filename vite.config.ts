4cf5cd0a57b0b717036bfec37f32d24e376f9bba
karthik <krk0241@gmail.com>
2025-05-02 12:43:52 -0400

    add homepage

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/resturent-page',
+  base: '/resturent-page/',
   server: {
     host: "::",
     port: 8080,
